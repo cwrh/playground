439eb51b7381e9be9c767ed7baa47a01424ab8c9
Christian Ritter-Holmes <christianritterholmes@gmail.com>
2017-10-23 23:45:26 -0500

    '

--- scripts/observe.js
+++ scripts/observe.js
@@ -1,39 +1,38 @@
 // Pub/Sub implementation to handle UI event firing/listening
 var Switchboard = {
-  switchList: {},
+	switchList: {},
 
-  subscribe: function ( switchId, listener ) {
-    // create the switchId if not yet created
-    if ( !this.switchList[ switchId ] ) this.switchList[ switchId ] = [];
+	subscribe: function(switchId, listener) {
+		// create the switchId if not yet created
+		if (!this.switchList[switchId]) this.switchList[switchId] = [];
 
-    // add the listener
-    this.switchList[ switchId ].push( listener );
-  },
+		// add the listener
+		this.switchList[switchId].push(listener);
+	},
 
-  publish: function ( switchId, status ) {
-    // return if the switchId doesn't exist, or there are no listeners
-    if ( !this.switchList[ switchId ] || this.switchList[ switchId ].length < 1 ) return;
+	publish: function(switchId, status) {
+		// return if the switchId doesn't exist, or there are no listeners
+		if (!this.switchList[switchId] || this.switchList[switchId].length < 1) return;
 
-    // send the event to all listeners
-    this.switchList[ switchId ].forEach( function ( listener ) {
-      listener( status || {} );
-    } );
-  }
+		// send the event to all listeners
+		this.switchList[switchId].forEach(function(listener) {
+			listener(status || {});
+		});
+	}
 };
 // Independent on/off switches ( i.e. input[type="checkbox"] )
-var SoloSwitch = function ( id ) {
-  this.id = id;
-  this.checkbox = document.getElementById( id );
+var CheckboxSwitch = function(id) {
+	this.checkbox = document.getElementById(id);
 };
-SoloSwitch.prototype = {
-  toggle: function () {
-    if ( this.checkbox.checked ) {
-      return this.checkbox.checked = false;
-    }
-    return this.checkbox.checked = true;
-  }
+CheckboxSwitch.prototype = {
+	flipSwitch: function() {
+		if (this.checkbox.checked) {
+			return this.checkbox.checked = false;
+		}
+		return this.checkbox.checked = true;
+	}
 };
 // Single-selection groups of switches ( i.e. input[type="radio"] )
 var MultiSwitch = {};
 
-var drawerSwitch = new SoloSwitch( "drawer-switch" );
+var drawerSwitch = new CheckboxSwitch("drawer-switch");
