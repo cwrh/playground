f2982b95e6ec48921a9bdf518929823a1a1341a1
Christian Ritter-Holmes <christianritterholmes@gmail.com>
2017-10-28 01:24:32 -0500

    k

--- scripts/observe.js
+++ scripts/observe.js
@@ -1,84 +1,80 @@
-function extend( obj, extension ) {
-  for ( var key in extension ) {
-    obj[ key ] = extension[ key ];
-  }
+function extend(obj, extension) {
+	for (var key in extension) {
+		obj[key] = extension[key];
+	}
 }
 
 function ObserverList() {
-  this.observerList = [];
+	this.observerList = [];
 };
 
 ObserverList.prototype = {
-  add: function ( obj ) {
-    return this.observerList.push( obj );
-  },
+	add: function(obj) {
+		return this.observerList.push(obj);
+	},
 
-  count: this.length = function () {
-    return this.observerList.length;
-  },
+	count: this.length = function() {
+		return this.observerList.length;
+	},
 
-  get: function ( index ) {
-    if ( index > -1 && index < this.observerList.length ) {
-      return this.observerList[ index ];
-    }
-  },
+	get: function(index) {
+		if (index > -1 && index < this.observerList.length) {
+			return this.observerList[index];
+		}
+	},
 
-  indexOf: function ( obj, startIndex ) {
-    var i = startIndex;
+	indexOf: function(obj, startIndex) {
+		var i = startIndex;
 
-    while ( i < this.observerList.length ) {
-      if ( this.observerList[ i ] === obj ) {
-        return i;
-      }
-      i++;
-    }
+		while (i < this.observerList.length) {
+			if (this.observerList[i] === obj) {
+				return i;
+			}
+			i++;
+		}
 
-    return -1;
-  },
+		return -1;
+	},
 
-  removeAt: function ( index ) {
-    this.observerList.splice( index, 1 );
-  }
+	removeAt: function(index) {
+		this.observerList.splice(index, 1);
+	}
 }
 
 function Observer() {
-  this.update = function () {
-    console.log( 'Observer update function...' );
-  }
+	this.update = function() {
+		console.log('Observer update function...');
+	}
 }
 
 function Subject() {
-  this.observers = new ObserverList();
+	this.observers = new ObserverList();
 }
 
 Subject.prototype = {
-  addObserver: function ( observer ) {
-    this.observers.add( observer );
-  },
+	addObserver: function(observer) {
+		this.observers.add(observer);
+	},
 
-  removeObserver: function ( observer ) {
-    this.observers.removeAt( this.observers.indexOf( observer, 0 ) );
-  },
+	removeObserver: function(observer) {
+		this.observers.removeAt(this.observers.indexOf(observer, 0));
+	},
 
-  notify: function ( context ) {
-    var observerCount = this.observers.count();
-    for ( var i = 0; i < observerCount; i++ ) {
-      this.observers.get( i ).update( context );
-    }
-  }
+	notify: function(context) {
+		var observerCount = this.observers.count();
+		for (var i = 0; i < observerCount; i++) {
+			this.observers.get(i).update(context);
+		}
+	}
 };
 
-var drawerSwitch = document.getElementById( 'drawer-toggle' );
-extend( drawerSwitch, new Subject() );
-drawerSwitch.onclick = function () {
-  drawerSwitch.notify( drawerSwitch.checked );
+var DrawerSwitch = document.getElementById('drawer-switch');
+
+/*	Concrete Subjects & Observers		*/
+extend(DrawerSwitch, new Subject());
+
+
+DrawerSwitch.onclick = function() {
+	var isChecked = this.checked;
+	this.notify(isChecked);
 };
-var navMenuItems = document.querySelectorAll( '.nav-menu-item' );
-navMenuItems.forEach( item => {
-  extend( item, new Observer() );
-  item.update = function ( context ) {
-    if ( context === true ) {
-      return item.checked = false;
-    }
-  }
-} )
--- scripts/pubsub.js
+++ scripts/pubsub.js
@@ -1,82 +1,89 @@
 var pubsub = {};
 
-( function ( myObject ) {
+(function(myObject) {
 
-  // Storage for topics that can be broadcast
-  // or listened to
-  var topics = {};
+	// Storage for topics that can be broadcast
+	// or listened to
+	var topics = {};
 
-  // A topic identifier
-  var subUid = -1;
+	// A topic identifier
+	var subUid = -1;
 
-  // Publish or broadcast events of interest
-  // with a specific topic name and arguments
-  // such as the data to pass along
-  myObject.publish = function ( topic, args ) {
+	// Publish or broadcast events of interest
+	// with a specific topic name and arguments
+	// such as the data to pass along
+	myObject.publish = function(topic, args) {
 
-    if ( !topics[ topic ] ) {
-      return false;
-    }
+		if (!topics[topic]) {
+			return false;
+		}
 
-    var subscribers = topics[ topic ],
-      len = subscribers ? subscribers.length : 0;
+		var subscribers = topics[topic],
+			len = subscribers ? subscribers.length : 0;
 
-    while ( len-- ) {
-      subscribers[ len ].func( topic, args );
-    }
+		while (len--) {
+			subscribers[len].func(topic, args);
+		}
 
-    return this;
-  };
+		return this;
+	};
 
-  // Subscribe to events of interest
-  // with a specific topic name and a
-  // callback function, to be executed
-  // when the topic/event is observed
-  myObject.subscribe = function ( topic, func ) {
+	// Subscribe to events of interest
+	// with a specific topic name and a
+	// callback function, to be executed
+	// when the topic/event is observed
+	myObject.subscribe = function(topic, func) {
 
-    if ( !topics[ topic ] ) {
-      topics[ topic ] = [];
-    }
+		if (!topics[topic]) {
+			topics[topic] = [];
+		}
 
-    var token = ( ++subUid ).toString();
-    topics[ topic ].push( {
-      token: token,
-      func: func
-    } );
-    return token;
-  };
+		if (typeOf func !== function) {
+			return false;
+		}
 
-  // Unsubscribe from a specific
-  // topic, based on a tokenized reference
-  // to the subscription
-  myObject.unsubscribe = function ( token ) {
-    for ( var m in topics ) {
-      if ( topics[ m ] ) {
-        for ( var i = 0, j = topics[ m ].length; i < j; i++ ) {
-          if ( topics[ m ][ i ].token === token ) {
-            topics[ m ].splice( i, 1 );
-            return token;
-          }
-        }
-      }
-    }
-    return this;
-  };
-}( pubsub ) );
+		var token = (++subUid).toString();
+		topics[topic].push({
+			token: token,
+			func: func
+		});
+		return token;
+	};
 
-var drawerMainSwitch = document.getElementById( 'drawer-switch' );
-var drawerSubscriber = function ( topic, data ) {
-  var check = data;
-  if ( !check ) {
-    document.getElementById( 'drawer-switch' ).checked = true;
-  }
-  document.getElementById( 'drawer-switch' ).checked = false
+	// Unsubscribe from a specific
+	// topic, based on a tokenized reference
+	// to the subscription
+	myObject.unsubscribe = function(token) {
+		for (var m in topics) {
+			if (topics[m]) {
+				for (var i = 0, j = topics[m].length; i < j; i++) {
+					if (topics[m][i].token === token) {
+						topics[m].splice(i, 1);
+						return token;
+					}
+				}
+			}
+		}
+		return this;
+	};
+}(pubsub));
+
+/*
+ * Select for label element siblings of radio/checkbox input elements
+ * as input elements themselves are hidden & unclickable
+ */
+var DrawerSwitch = document.querySelector('#drawer-switch + label');
+
+var DrawerSubscriber = function(topic, data) {
+	if (!data) {
+		document.getElementById('drawer-switch').checked = true;
+	}
+	document.getElementById('drawer-switch').checked = false
 };
-var drawerSubToken = pubsub.subscribe( 'Drawer', drawerSubscriber );
-var drawerNavSubscriber = function ( topic, data ) {
-  let navIsChecked = data.checked,
-    navId = data.id;
 
-  drawerMainSwitch.checked = false;
+var DrawerSubscription = pubsub.subscribe('drawer.switch', DrawerSubscriber);
 
-}
+DrawerSwitch.onclick = () => {
+	var DrawerIsOpen = document.getElementById('drawer-switch').checked;
+	pubsub.publish('drawer.switch', DrawerIsOpen);
+};
