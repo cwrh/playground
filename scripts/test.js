aba856026905269329bb40d826b122cb24a9f7a0
Christian Ritter-Holmes <christianritterholmes@gmail.com>
2017-11-04 03:19:05 -0500

    k

--- scripts/test.js
+++ scripts/test.js
@@ -1,76 +1,110 @@
 var EventManager = {};
 
-( function ( o ) {
+(function(o) {
 
-  // Storage for topics that can be broadcast or listened to
-  var broadcasts = {};
+	// Storage for topics that can be broadcast or listened to
+	var broadcasts = {};
 
-  // A unique tokenized identifier for individual subscribers
-  var subToken = -1;
+	// A unique tokenized identifier for individual subscribers
+	var subToken = -1;
 
-  // Publish or broadcast events of interest
-  // with a specific topic name and arguments
-  // such as the data to pass along
-  o.publish = o.pub = function ( topic, args ) {
+	// Publish or broadcast events of interest
+	// with a specific topic name and arguments
+	// such as the data to pass along
+	o.broadcast = o.cast = function(message, args) {
 
-    if ( !broadcasts[ topic ] ) {
-      return false;
-    }
+		if (!broadcasts[message]) {
+			return false;
+		};
 
-    var subscribers = broadcasts[ topic ],
-      len = subscribers ? subscribers.length : 0;
+		var subscribers = broadcasts[message],
+			len = subscribers ? subscribers.length : 0;
 
-    while ( len-- ) {
-      subscribers[ len ].func( topic, args );
-    }
+		while (len--) {
+			subscribers[len].func(message, args);
+		};
 
-    return this;
-  };
+		return this;
+	};
 
-  // Subscribe to events of interest
-  // with a specific topic name and a
-  // callback function, to be executed
-  // when the topic/event is observed
-  o.subscribe = o.sub = function ( topic, func ) {
+	o.sendBroadcast = function(receiver, message, data) {
+		var receivers = broadcasts[message],
+			b;
 
-    if ( !broadcasts[ topic ] ) {
-      broadcasts[ topic ] = [];
-    };
+		if (!broadcasts.hasOwnProperty(receiver)) {
+			return;
+		};
 
-    if ( typeOf func !== function ) {
-      return false;
-    };
+		for (b in broadcasts) {
+			if (receivers.hasOwnProperty(b)) {
+				sendBroadcast(receivers[b], message, data);
+			}
+		}
+	};
 
-    var token = ( ++subUid ).toString();
-    broadcasts[ topic ].push( {
-      token: token,
-      func: func
-    } );
-    return token;
-  };
+	o.subtopics = function(receiver, message, data) {
+		var submessage = String(message),
+			position = topic.lastIndexOf('.');
 
-  // Unsubscribe from a specific
-  // topic, based on a tokenized reference
-  // to the subscription
-  o.unsubscribe = o.unsub = function ( token ) {
-    for ( var m in broadcasts ) {
-      if ( broadcasts[ m ] ) {
-        for ( var i = 0, j = broadcasts[ m ].length; i < j; i++ ) {
-          if ( broadcasts[ m ][ i ].token === token ) {
-            broadcasts[ m ].splice( i, 1 );
-            return token;
-          }
-        }
-      }
-    }
-    return this;
-  };
-}( EventManager ) );
+		// deliver the message as it is now
+		sendBroadcast(receiver, message, data);
 
-var uiDrawer = document.getElementById( 'drawer' );
-var drawerNavItems = drawerElement.querySelectorAll( 'input[type="radio"]' );
-var drawerHandle = document.getElementById( 'drawer-handle' );
+		// trim the hierarchy and deliver message to each level
+		while (position !== -1) {
+			submessage = submessage.substr(0, position);
+			position = submessage.lastIndexOf('.');
+			sendBroadcast(receiver, submessage, data);
+		};
+	};
 
-function drawerSubscriberFunction() {
+	// Subscribe to events of interest
+	// with a specific topic name and a
+	// callback function, to be executed
+	// when the topic/event is observed
+	o.subscribe = o.sub = function(message, func) {
+
+		if (!broadcasts[message]) {
+			broadcasts[message] = [];
+		};
+
+		if (typeOf func !== function) {
+			return false;
+		};
+
+		var token = (++subUid).toString();
+		broadcasts[message].push({
+			token: token,
+			func: func
+		});
+		return token;
+	};
+
+	// Unsubscribe from a specific
+	// topic, based on a tokenized reference
+	// to the subscription
+	o.unsubscribe = o.unsub = function(token) {
+		for (var m in broadcasts) {
+			if (broadcasts[m]) {
+				for (var i = 0, j = broadcasts[m].length; i < j; i++) {
+					if (broadcasts[m][i].token === token) {
+						broadcasts[m].splice(i, 1);
+						return token;
+					}
+				}
+			}
+		}
+		return this;
+	};
+}(EventManager));
+
+var uiDrawer = document.getElementById('drawer');
+var drawerNavItems = drawerElement.querySelectorAll('input[type="radio"]');
+var drawerHandle = document.getElementById('drawer-handle');
+
+
+EventManager.subscribe('drawer.nav', drawer);
+EventManager.subscribe('drawer.handle', drawerReceiver);
+
+var drawerReceiver = function(message, data) {
 
 }
