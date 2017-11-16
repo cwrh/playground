bb108cc74e99439d402e667c6fe27efbda166e1c
Christian Ritter-Holmes <christianritterholmes@gmail.com>
2017-11-15 23:46:25 -0600

    l;

--- scripts/test.js
+++ scripts/test.js
@@ -1,106 +1,126 @@
-var EventManager = ( function () {
-  /*
-   *  EventManager ***
-   *  Handles all pub/sub events for the application UI.
-   *  Event Chain: "topLevelElement.action.targetElement"
-   */
-  // Top-level object used to store all subscriber/subscription data
-  var topics = {};
+var EventManager = ( function()
+{
+		/*
+		 *  EventManager ***
+		 *  Topic format: 'Event.Region.Element'
+		 *  ex. 'click.drawer.handle'   * *  Event is always required, Regrion/Element are optional
+		 *  ex. 'dblclick.pusher'       * *  Region is only required if Element is specified
+		 *  ex. 'keydown'               * *  Event can be broadcast by itself
+		 */
+		// Top-level object used to store all subscriber/subscription data
+		var topics = {};
 
-  // A unique tokenized identifier for individual subscribers
-  var uid = -1;
+		// A unique tokenized identifier for individual subscribers
+		var uid = -1;
 
-  function hasSubtopic( topic ) {
-    if ( topic.indexOf( '.' ) !== -1 ) {
-      return true;
-    }
+		function hasSubtopic( topic )
+		{
+				if ( topic.indexOf( '.' ) !== -1 )
+				{
+						return true;
+				}
 
-    return false;
-  }
+				return false;
+		}
 
-  // Publish or broadcast events of interest
-  // with a specific topic name and arguments
-  // such as the data to pass along
-  function publish( topic, data ) {
-    if ( !hasSubtopic( topic ) ) {
-      disseminate( topic, data );
-    }
+		// Publish or broadcast events of interest
+		// with a specific topic name and arguments
+		// such as the data to pass along
+		function publish( topic, data )
+		{
+				if ( !hasSubtopic( topic ) )
+				{
+						disseminate( topic, data );
+				}
 
-    disseminate( topic, data );
-    var subtopic = topic.substring( ( topic.indexOf( '.' ) ), ( topic.length ) );
-    disseminate( subtopic, data );
-  }
+				disseminate( topic, data );
+				var subtopic = topic.substring( ( topic.indexOf( '.' ) ), ( topic.length ) );
+				disseminate( subtopic, data );
+		}
 
-  function disseminate( topic, data ) {
+		function disseminate( topic, data )
+		{
 
-    // Abort if there are no subscribers to the event
-    if ( !topics[ topic ] ) {
-      return;
-    }
+				// Abort if there are no subscribers to the event
+				if ( !topics[ topic ] )
+				{
+						return;
+				}
 
-    // Iterate through subscribers
-    topics[ topic ].forEach( function ( subscriberFn ) {
-      // Abort if data is undefined
-      if ( !data ) {
-        return;
-      }
+				// Iterate through subscribers
+				topics[ topic ].forEach( function( subscriberFn )
+				{
+						// Abort if data is undefined
+						if ( !data )
+						{
+								return;
+						}
 
-      // If data exists, pass it to the subscriber function
-      subscriberFn( topic, data );
-    } );
+						// If data exists, pass it to the subscriber function
+						subscriberFn( topic, data );
+				} );
 
-    return this;
-  }
+				return this;
+		}
 
-  // Subscribe to events of interest
-  // with a specific topic name and a
-  // callback function, to be executed
-  // when the topic/event is observed
-  function subscribe( topic, fn ) {
-    var token = 'UID_' + ( ++uid ).toString();
+		// Subscribe to events of interest
+		// with a specific topic name and a
+		// callback function, to be executed
+		// when the topic/event is observed
+		function subscribe( topic, fn )
+		{
+				var token = 'UID_' + ( ++uid ).toString();
 
-    if ( !topics[ topic ] ) {
-      topics[ topic ] = [];
-    }
+				if ( !topics[ topic ] )
+				{
+						topics[ topic ] = [];
+				}
 
-    if ( typeOf fn !== 'function' ) {
-      return;
-    }
+				if ( typeOf fn !== 'function' )
+				{
+						return;
+				}
 
-    topics[ topic ].push( {
-      uid: token,
-      func: fn,
-    } );
+				topics[ topic ].push(
+				{
+						uid: token,
+						func: fn,
+				} );
 
-    return {
-      uid: token,
-      func: fn,
-      subscription: topic
-    };
-  }
+				return {
+						uid: token,
+						func: fn,
+						subscription: topic
+				};
+		}
 
-  // Unsubscribe from a specific
-  // topic, based on a tokenized reference
-  // to the subscription
-  function unsubscribe( token ) {
-    for ( var m in topics ) {
-      if ( topics[ m ] ) {
-        for ( var i = 0, j = topics[ m ].length; i < j; i++ ) {
-          if ( topics[ m ][ i ].token === token ) {
-            topics[ m ].splice( i, 1 );
-            return token;
-          }
-        }
-      }
-    }
-    return this;
-  }
+		// Unsubscribe from a specific
+		// topic, based on a tokenized reference
+		// to the subscription
+		function unsubscribe( token )
+		{
+				for ( var m in topics )
+				{
+						if ( topics[ m ] )
+						{
+								for ( var i = 0, j = topics[ m ].length; i < j; i++ )
+								{
+										if ( topics[ m ][ i ].token === token )
+										{
+												topics[ m ].splice( i, 1 );
+												return token;
+										}
+								}
+						}
+				}
+				return this;
+		}
 
-  return {
-    publish: publish,
-    subscribe: subscribe,
-    unsubscribe: unsubscribe
-  };
+		return {
+				publish: publish,
+				subscribe: subscribe,
+				unsubscribe: unsubscribe
+		};
 }() );
 
 var Drawer = document.getElementById( 'drawer' );
@@ -111,18 +131,22 @@ EventManager.subscribe( 'click.drawer', drawerSubscriber );
 
 drawerHandle.onclick = publish( 'click.drawer.handle', this )
 
-var drawerSubscriber = function ( topic, data ) {
-  if ( !data ) {
-    return;
-  }
+var drawerSubscriber = function( topic, data )
+{
+		if ( !data )
+		{
+				return;
+		}
 
-  let clicked = data,
-    broadcast = topic;
+		let clicked = data,
+				broadcast = topic;
 
-  if ( clicked.tagName === 'INPUT' && clicked.checked === true ) {
-    if ( clicked.classList.contains( 'sb-radio' ) ) {
-      // Focus: selected page
-    }
+		if ( clicked.tagName === 'INPUT' && clicked.checked === true )
+		{
+				if ( clicked.classList.contains( 'sb-radio' ) )
+				{
+						// Focus: selected page
+				}
 
-  }
+		}
 };
